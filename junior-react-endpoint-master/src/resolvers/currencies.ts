01d8ab723f81b0487c6f8a2af3f9212306b43e22
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2022-09-27 21:24:18 +0200

    restructure project

--- junior-react-endpoint-master/src/resolvers/currencies.ts
+++
@@ -1,6 +0,0 @@
-import converter from '../util/currency-converter';
-import { Currency } from '../data/type';
-
-const currencyResolver = (): Currency[] => converter.availableCurrencies;
-
-export default currencyResolver;
